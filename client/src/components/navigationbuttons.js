343ac0e56fe7dbf4cc83acfd9e64cdd7229a9e38
Harold Davidson <harold.davidson89@gmail.com>
2018-06-28 12:05:57 -0500

    drawer mostly done

--- client/src/components/navigationbuttons.js
+++ client/src/components/navigationbuttons.js
@@ -10,15 +10,15 @@ const NavButtons = (props) => {
         <div className="container">
             <div className="row">
             <div className="col-md-4 col-sm-4">
-                    <button onClick={() => props.onOpenModal()} type="button" className="btn btn-outline-success nav-buttons">{props.userName}</button>
+                    <button onClick={() => props.onOpenModal()} type="button" className="btn btn-outline-light nav-buttons" active="none">{props.userName}</button>
                 </div>
                 <div className="col-md-4 col-sm-4">
                 <a href="mailto:sweetideabakery@gmail.com">
-                    <button type="button" className="btn btn-outline-success nav-buttons">Contact Us</button>
+                    <button type="button" className="btn btn-outline-light nav-buttons">Contact Us</button>
                 </a>
                 </div>
                 <div className="col-md-4 col-sm-4">
-                    <button type="button" className="btn btn-outline-success nav-buttons">Shopping Cart</button>
+                    <button onClick={() => props.toggleSideBar()} type="button" className="btn btn-outline-light nav-buttons">Shopping Cart</button>
                 </div>
                     
                 
