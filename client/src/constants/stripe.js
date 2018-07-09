09e77ec0b3289564b5ada9f6e3d2955eeb74990d
Pat Shehan <pshehan@gmail.com>
2018-07-08 23:24:57 -0500

    ignore

--- client/src/constants/stripe.js
+++ client/src/constants/stripe.js
@@ -1,5 +1,5 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_live_dnDzNRnVhvOjUBIdqaxeg6rI'
+  ? 'pk_live_fcVEWSb208V00jHAyPdvPy5n'
   : 'pk_test_dnDzNRnVhvOjUBIdqaxeg6rI';
 
 export default STRIPE_PUBLISHABLE;
--- constants/stripe.js
+++
@@ -1,9 +0,0 @@
-const configureStripe = require('stripe');
-
-const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? 'sk_live_o2b2rub1sWQFmWZmBEeAlzIE'
-    : 'sk_test_nMZS9SqX05RYtdqlndv8OWGG';
-
-const stripe = configureStripe(STRIPE_SECRET_KEY);
-
-module.exports = stripe;
