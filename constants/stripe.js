bc2364eec21d8ac35989ce44196e8daa674b2cc1
Pat Shehan <pshehan@gmail.com>
2018-07-08 23:15:56 -0500

    constants/stripe to gitignore

--- constants/stripe.js
+++ constants/stripe.js
@@ -1,7 +1,7 @@
 const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
-    ? 'sk_live_nMZS9SqX05RYtdqlndv8OWGG'
+    ? 'sk_live_o2b2rub1sWQFmWZmBEeAlzIE'
     : 'sk_test_nMZS9SqX05RYtdqlndv8OWGG';
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
