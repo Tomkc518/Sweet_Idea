c2b1ce0cbdb0df723d8fe0acdf52ddeb4663aaf4
Thomas Fuller <tomkc518@yahoo.com>
2018-07-10 21:21:53 -0500

    adding fix

--- constants/frontend.js
+++ constants/frontend.js
@@ -3,10 +3,10 @@ const FRONTEND_DEV_URLS = [ 'http://localhost:3000', 'http://localhost:3001/'];
 const FRONTEND_PROD_URLS = [
   'https://www.sweet-idea.herokuapp.com/',
   'https://sweet-idea.herokuapp.com/',
-  'https://sweet-idea.herokuapp.com/api/account/signup*',
-  'https://sweet-idea.herokuapp.com/api/account/signin*',
-  'https://sweet-idea.herokuapp.com/api/account/verify*',
-  'https://sweet-idea.herokuapp.com/api/account/logout*',
+  // 'https://sweet-idea.herokuapp.com/api/account/signup*',
+  // 'https://sweet-idea.herokuapp.com/api/account/signin*',
+  // 'https://sweet-idea.herokuapp.com/api/account/verify*',
+  // 'https://sweet-idea.herokuapp.com/api/account/logout*',
 ];
 
 module.exports = process.env.NODE_ENV === 'production'
